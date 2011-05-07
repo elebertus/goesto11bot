d5b111d69a0bce2693fc91d4009ec08f23abe1c7
Erik Hollensbe <erik@hollensbe.org>
2011-05-07 10:17:19 -0400

    redis-backed now.

--- loud.rb
+++ loud.rb
@@ -1,27 +1,39 @@
-require 'sqlite3'
+require 'redis'
 
 module Cinch::Plugins
   module LOUD
-    class DATABASE
+    class REDIS
 
-      LOUDDB = "louds.db"
-
-      INIT_SQL = [
-        %q[create table louds (id integer primary key autoincrement, loud varchar not null, rating integer default 1)]
-      ]
-
-      def initialize
-        do_init_db = !File.exist?(LOUDDB)
-        @db = SQLite3::Database.new("louds.db")
-        init_db if do_init_db
+      class << self
+        attr_accessor :last_loud
       end
 
-      def init_db
-        INIT_SQL.each { |sql| @db.execute(sql) }
+      def initialize
+        @db = Redis.new(:path => 'redis.sock')
+        self.class.last_loud = randomloud
       end
 
       def add_loud(loud)
-        @db.execute("insert into louds (loud) values (?)", loud)
+        @db.pipelined do
+          @db.setnx loud, 1
+          @db.persist loud
+        end
+      end
+
+      def randomloud
+        self.class.last_loud = @db.randomkey
+      end
+
+      def bump
+        @db.incr self.class.last_loud
+      end
+
+      def sage
+        @db.decr self.class.last_loud
+      end
+
+      def score
+        return "#{self.class.last_loud}: #{@db.get(self.class.last_loud)}"
       end
     end
 
@@ -32,14 +44,39 @@ module Cinch::Plugins
 
       def initialize(*args)
         super *args
-        @db = DATABASE.new
+        @db = REDIS.new
       end
 
       match %r/^([A-Z\W]+)$/, :use_prefix => false, :use_suffix => false
+      react_on :channel
 
       def execute(m, query)
         if query.length >= MIN_LENGTH and query =~ /[A-Z]/ and query.scan(/[A-Z]/).length > query.scan(/\W/).length
           @db.add_loud(query)
+          m.reply(@db.randomloud)
+        end
+      end
+    end
+
+    class LOUDMETA
+      include Cinch::Plugin
+
+      def initialize(*args)
+        super *args
+        @db = REDIS.new
+      end
+
+      match %r/(bump|sage|score)$/, :use_prefix => true, :use_suffix => false
+      react_on :channel
+
+      def execute(m, query)
+        case query
+        when 'bump'
+          @db.bump
+        when 'sage'
+          @db.sage
+        when 'score'
+          m.reply(@db.score)
         end
       end
     end
